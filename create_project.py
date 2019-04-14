fc7d0942857f35510bb932f8bb46c5a8f5d7762b
Qewbicle <qewbicle@users.noreply.github.com>
2019-04-14 12:58:50 -0700

    Update create_project.py
    Created a settings area.
    
    Added a file_suffix, that gets appended to file name. This also helps when accidentally pressing enter when inputting a name, by default the name will be just the file_suffix.
    
    Moved directory creator into main function.
    
    Added handling of situation where the venv exists in the creation folder.
    
    Now prints out a success or error message if venv exists.

--- create_project.py
+++ create_project.py
@@ -1,24 +1,43 @@
 import os
 
+#Settings
 user_directory = os.path.expanduser("~")
 venv_directory = user_directory + "\\python_venvs"
+file_suffix = "_venv"
 launcher_location = user_directory + "\\Desktop"
 
-if not os.path.exists(venv_directory):
-    os.makedirs(venv_directory)
 
 def main():
     """
     Creates a python virtual environment in a new folder called 'python_venvs'
     Creates a .bat file on the desktop used as a shortcut to launch the venv
     """
+    #Create directory where all venvs will be created in if it doesn't exist
+    if not os.path.exists(venv_directory):
+        os.makedirs(venv_directory)
+    
     project_name = input("Enter project name:\n")
     os.chdir(venv_directory)
-    os.system(f"python -m venv {project_name}venv")
-    os.chdir(launcher_location)
+    
+    #Check to see if project already exists, if so, give a warning
+    if not os.path.exists(venv_directory + f"\\{project_name}{file_suffix}"):
+        os.system(f"python -m venv {project_name}{file_suffix}")
+        os.chdir(launcher_location)
+        #
+        with open(f"{project_name}{file_suffix}.bat", "w") as bat_file:
+            bat_file.write(f'@echo off\ncmd /k"cd {venv_directory}\\{project_name}{file_suffix} & {venv_directory}\\{project_name}{file_suffix}\\Scripts\\activate"')
+        print(f"""
+Success.
+Project: "{project_name}{file_suffix}" created. 
+""")
+    else:
+        print(f"""
+Project Name "{project_name}{file_suffix}" already exists.
 
-    with open(f"{project_name}.bat", "w") as bat_file:
-        bat_file.write(f'@echo off\ncmd /k"cd {venv_directory}\\{project_name}venv & {venv_directory}\\{project_name}venv\\Scripts\\activate"')
+To start fresh, please delete project folder "{project_name}{file_suffix}" in:
+    "{venv_directory}"
+""")
+    input("Press Enter to exit.")
 
 
 if __name__ == '__main__':
